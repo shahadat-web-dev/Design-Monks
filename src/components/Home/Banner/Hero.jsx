b944194c9fc51442a8c3d57e0e91ca3c74070efc
shahadat-web-dev <misssharamoni@gamil.com>
2026-03-22 14:27:27 +0600

    image infinite animation done

--- src/components/Home/Banner/Hero.jsx
+++ src/components/Home/Banner/Hero.jsx
@@ -9,23 +9,23 @@ const Hero = () => {
 
         {/* left image */}
         <div>
-          <img className='' src="/public/left.png" alt="" />
+          <img className='float-animation hidden md:block' src="/public/left.png" alt="" />
         </div>
 
         {/* Center Text */}
-        <div className=''>
-          <div className='pl-82'>
+        <div className='md:pr-10 '>
+          <div className='lg:pl-80 md:pl-10 pl-30'>
             <img className='w-30' src="/google.png" alt="" />
             <h4 className='text-white text-sm'>4.9 ⭐⭐⭐⭐⭐</h4>
           </div>
-          <h1 className='text-white pt-4 text-xl font-semibold pl-60'>Leading UI/UX Design Agency</h1>
-          <h1 className='text-white pt-5 font-bold text-[68px]'>We <span className='italic playfair'>Design</span> Products That <br />
+          <h1 className='text-white pt-4 lg:text-xl font-semibold lg:pl-60 pl-15'>Leading UI/UX Design Agency</h1>
+          <h1 className='text-white pt-5 font-bold text-3xl lg:text-[60px] md:text-2xl'>We <span className='italic playfair'>Design</span> Products That <br />
             Drive <span className='italic playfair'>Results</span></h1>
         </div>
 
         {/* Right image */}
         <div className=''>
-          <img className='' src="/right.png" alt="" />
+          <img className='float-animation hidden md:block' src="/right.png" alt="" />
         </div>
 
       </div>
